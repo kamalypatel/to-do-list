4f04b583cf01b7089fb1f6829e365d71253ff8bf
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-04-25 18:10:08 -0400

    fixing bugs with the display for to-do-items

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function displayCategory(categoryName) {
     const leftSection = document.querySelector('.leftSection')
     const toDoListCategory = document.createElement('div')
-    toDoListCategory.classList.add('catergorySelector')
+    toDoListCategory.classList.add('categorySelector')
     toDoListCategory.innerText = categoryName
     leftSection.appendChild(toDoListCategory)
 }
@@ -17,10 +17,10 @@ const createCategory = ( (categoryName) => {
     let getToDoList = () => toDoList;
 
     let displayToDoList = () => {
-        const middleSection = document.querySelector('middleSection')
+        const middleSection = document.querySelector('.middleSection')
         middleSection.innerHTML = ''
-        for (let i = 0; i <= toDoList.length - 1; i++) {
-            const middleSection = document.querySelector('middleSection')
+        for (let i = 0; i < toDoList.length; i++) {
+            const middleSection = document.querySelector('.middleSection')
             const toDoListItems = document.createElement('div')
             toDoListItems.innerText = toDoList[i].toDoName
             middleSection.appendChild(toDoListItems)
@@ -33,9 +33,9 @@ const createCategory = ( (categoryName) => {
 
 let general = createCategory('General')
 let first = general.toDoListItem('call doc', 'call 716-970-8530', 'tomorrow', 'priority')
-
+let second = general.toDoListItem('hello')
 
 console.log(general.categoryName)
-console.log(general.getToDoList)
+console.log(general.getToDoList())
 console.log(first.toDoName)
 general.displayToDoList()
